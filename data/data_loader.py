6cd1496dfbb848cf43e564cf686052de0ab05907
Sharan <sharan.nandkumar@gmail.com>
2023-07-29 13:48:29 +0200

    debug

--- data/data_loader.py
+++ data/data_loader.py
@@ -3,7 +3,11 @@ import os
 
 def load_players_data():
     current_dir = os.path.dirname(os.path.abspath(__file__))
+    st.write("Current Directory:", current_dir)
+    
     file_path = os.path.join(current_dir, 'players.csv')
-    players_df = pd.read_csv('players.csv')
+    st.write("File Path:", file_path)
+    
+    players_df = pd.read_csv(file_path)
     # Perform any additional data processing if needed
     return players_df

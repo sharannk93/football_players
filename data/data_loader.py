7d539718204369c4e2b297efd76ab68fdcaa8e7c
Sharan <sharan.nandkumar@gmail.com>
2023-07-29 13:35:22 +0200

    bug fix 3

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def load_players_data():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'players.csv')
     players_df = pd.read_csv('players.csv')
     # Perform any additional data processing if needed
     return players_df
